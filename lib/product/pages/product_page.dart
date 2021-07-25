2742f565935c7cb3ee14efcf12f5cc6335cc29b0
soundring <soundringing@yahoo.co.jp>
2021-07-26 01:31:50 +0900

    :wrench:Tabごとに表示する商品を変えるように

--- lib/product/pages/product_page.dart
+++ lib/product/pages/product_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:murasame_ec/product/model/model.dart';
 import 'package:murasame_ec/product/widgets/widget.dart';
 
 class ProductPage extends StatefulWidget {
@@ -14,7 +17,7 @@ class _ProductPageState extends State<ProductPage>
 
   @override
   void initState() {
-    _tabController = TabController(length: 4, vsync: this);
+    _tabController = TabController(length: 3, vsync: this);
     super.initState();
   }
 
@@ -27,13 +30,39 @@ class _ProductPageState extends State<ProductPage>
           indicatorColor: Colors.brown,
           tabs: const <Tab>[
             Tab(text: '全商品'),
-            Tab(text: '新商品'),
-            Tab(text: 'ランキング'),
-            Tab(text: 'おすすめ'),
+            Tab(text: '人気商品'),
+            Tab(text: '新着商品'),
           ],
         ),
-        const ProductListWidget(),
+        _TabBarView(tabController: _tabController)
+        // const ProductListWidget(),
       ],
+    );
+  }
+}
+
+class _TabBarView extends HookWidget {
+  const _TabBarView({required this.tabController});
+
+  final TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    final products = useProvider(
+        productsProvider.select((products) => products.productStocks));
+
+    return Expanded(
+      child: TabBarView(controller: tabController, children: [
+        ProductListWidget(
+          products: products,
+        ),
+        ProductListWidget(
+          products: products.where((products) => products.star > 4.5).toList(),
+        ),
+        ProductListWidget(
+          products: products.reversed.take(3).toList(),
+        ),
+      ]),
     );
   }
 }
--- lib/product/widgets/product_list_widget.dart
+++ lib/product/widgets/product_list_widget.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:murasame_ec/product/model/model.dart';
 import 'package:murasame_ec/product/widgets/widget.dart';
 
 class ProductListWidget extends HookWidget {
-  const ProductListWidget({Key? key}) : super(key: key);
+  const ProductListWidget({required this.products, Key? key}) : super(key: key);
+
+  final List<Product> products;
 
   @override
   Widget build(BuildContext context) {
-    final products = useProvider(
-        productsProvider.select((products) => products.productStocks.toList()));
-
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: GridView.builder(
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 6,
-          ),
-          itemCount: products.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ProductListTileWidget(
-              product: products[index],
-            );
-          },
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: GridView.builder(
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 6,
         ),
+        itemCount: products.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ProductListTileWidget(
+            product: products[index],
+          );
+        },
       ),
     );
   }
