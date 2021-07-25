00f53ef2967235439efda2b2959c00ff2e2763df
soundring <soundringing@yahoo.co.jp>
2021-07-26 00:19:40 +0900

    :wrench:jsonで読み込んだ商品情報を一覧表示するように

--- lib/product/widgets/product_list_widget.dart
+++ lib/product/widgets/product_list_widget.dart
@@ -1,80 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:murasame_ec/product/model/model.dart';
+import 'package:murasame_ec/product/widgets/widget.dart';
 
-class ProductListWidget extends StatelessWidget {
+class ProductListWidget extends HookWidget {
   const ProductListWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      padding: const EdgeInsets.all(5),
-      crossAxisCount: 2,
-      shrinkWrap: true,
-      childAspectRatio: 0.75,
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: Column(children: <Widget>[
-            Expanded(
-              child: Image.asset(
-                'product_data/images/chair1.jpg',
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'ゆったり座れるイス',
-                style: TextStyle(color: Colors.black.withOpacity(0.8)),
-              ),
-            ),
-            Row(
-              children: const <Widget>[
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-                Icon(
-                  Icons.star_border,
-                  color: Colors.grey,
-                ),
-                SizedBox(width: 10),
-                Icon(
-                  Icons.messenger,
-                  color: Color(0xffAEAB92),
-                ),
-                SizedBox(width: 3),
-                Text(
-                  '1件',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                )
-              ],
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                '¥3000(税込)',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ]),
+    final products = useProvider(
+        productsProvider.select((products) => products.productStocks.toList()));
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 6,
+          ),
+          itemCount: products.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ProductListTileWidget(
+              product: products[index],
+            );
+          },
         ),
-      ],
+      ),
     );
   }
 }
--- lib/product/widgets/widget.dart
+++ lib/product/widgets/widget.dart
@@ -1,1 +1,2 @@
+export 'package:murasame_ec/product/widgets/product_list_tile_widget.dart';
 export 'package:murasame_ec/product/widgets/product_list_widget.dart';
