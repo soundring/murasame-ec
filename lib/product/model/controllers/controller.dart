fd9f6aaabd5ae1cde0751d12de26b82590ce6aa1
soundring <soundringing@yahoo.co.jp>
2021-07-26 00:18:25 +0900

    :new:商品の状態を持つクラスと操作するコントローラーを作成

---
+++ lib/product/model/controllers/controller.dart
@@ -0,0 +1,1 @@
+export 'products_controller/products_controller.dart';
---
+++ lib/product/model/controllers/products_controller/products_controller.dart
@@ -0,0 +1,23 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:murasame_ec/product/model/model.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+import 'products_state.dart';
+
+final productsProvider =
+    StateNotifierProvider<ProductsController, ProductsState>(
+  (ref) => ProductsController(ref.read),
+);
+
+class ProductsController extends StateNotifier<ProductsState> {
+  ProductsController(this._read) : super(ProductsState()) {
+    () async {
+      state = state.copyWith(
+        productStocks: await _read(productsFetcher.future),
+        isLoading: false,
+      );
+    }();
+  }
+
+  final Reader _read;
+}
