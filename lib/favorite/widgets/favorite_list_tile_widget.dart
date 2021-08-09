0c7d882dd9324a0234fbc8e0a72ffb024192a774
soundring <soundringing@yahoo.co.jp>
2021-08-09 16:53:33 +0900

    :wrench:お気に入りリストの商品をタップしたら詳細ページに飛ぶように

--- lib/favorite/widgets/favorite_list_tile_widget.dart
+++ lib/favorite/widgets/favorite_list_tile_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:murasame_ec/product/model/entities/entities.dart';
+import 'package:murasame_ec/product/pages/pages.dart';
 import 'package:murasame_ec/product/widgets/widget.dart';
 
 class FavoriteListTileWidget extends StatelessWidget {
@@ -10,51 +11,60 @@ class FavoriteListTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            SizedBox(
-              width: 150,
-              child: Image.asset(favoriteProduct.image_path),
-            ),
-            Expanded(
-              child: Container(
-                height: 150,
-                padding: const EdgeInsets.only(top: 5, left: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      favoriteProduct.name,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.subtitle1,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    RatingBarWidget(product: favoriteProduct, itemSize: 24),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    ReviewCountWidget(
-                        reviewCount: favoriteProduct.review_count),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      '${favoriteProduct.price}円(税込)',
-                      style: Theme.of(context).textTheme.subtitle2,
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute<void>(builder: (context) {
+          return ProductDetailPage(
+            product: favoriteProduct,
+          );
+        }));
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 150,
+                child: Image.asset(favoriteProduct.image_path),
+              ),
+              Expanded(
+                child: Container(
+                  height: 150,
+                  padding: const EdgeInsets.only(top: 5, left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        favoriteProduct.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      RatingBarWidget(product: favoriteProduct, itemSize: 24),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      ReviewCountWidget(
+                          reviewCount: favoriteProduct.review_count),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '${favoriteProduct.price}円(税込)',
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-        const Divider(),
-      ],
+            ],
+          ),
+          const Divider(),
+        ],
+      ),
     );
   }
 }
