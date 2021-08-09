b1438b0e2dfdc8e7741b222ebb906b55f75b4491
soundring <soundringing@yahoo.co.jp>
2021-08-09 16:48:49 +0900

    :wrench:お気に入りリストのウィジェット切り出し

--- lib/favorite/widgets/favorite_list_widget.dart
+++ lib/favorite/widgets/favorite_list_widget.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:murasame_ec/favorite/model/controllers/controllers.dart';
+import 'package:murasame_ec/favorite/widgets/widgets.dart';
 import 'package:murasame_ec/product/model/controllers/controllers.dart';
 import 'package:murasame_ec/product/model/entities/entities.dart';
-import 'package:murasame_ec/product/widgets/rating_bar_widget.dart';
 
 class FavoriteListWidget extends HookWidget {
   const FavoriteListWidget({Key? key}) : super(key: key);
@@ -25,69 +25,14 @@ class FavoriteListWidget extends HookWidget {
       }
     }
 
-    return ListView.builder(
-        shrinkWrap: true,
-        itemCount: favoriteProducts.length,
-        itemBuilder: (BuildContext context, int index) {
-          final favoriteProduct = favoriteProducts[index];
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 150,
-                    child: Image.asset(favoriteProduct.image_path),
-                  ),
-                  Expanded(
-                    child: Container(
-                      height: 150,
-                      padding: const EdgeInsets.only(top: 5, left: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            favoriteProduct.name,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.subtitle1,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          RatingBarWidget(
-                              product: favoriteProduct, itemSize: 24),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.messenger,
-                                color: Color(0xffAEAB92),
-                                size: 26,
-                              ),
-                              Text(
-                                '${favoriteProduct.review_count}件',
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '${favoriteProduct.price}円(税込)',
-                            style: Theme.of(context).textTheme.subtitle2,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const Divider(),
-            ],
-          );
-        });
+    return favoriteProducts.isEmpty
+        ? const Text('お気に入りに登録された商品はまだありません')
+        : ListView.builder(
+            shrinkWrap: true,
+            itemCount: favoriteProducts.length,
+            itemBuilder: (BuildContext context, int index) {
+              final favoriteProduct = favoriteProducts[index];
+              return FavoriteListTileWidget(favoriteProduct: favoriteProduct);
+            });
   }
 }
--- lib/favorite/widgets/widgets.dart
+++ lib/favorite/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:murasame_ec/favorite/widgets/favorite_button_widget.dart';
+export 'package:murasame_ec/favorite/widgets/favorite_list_tile_widget.dart';
 export 'package:murasame_ec/favorite/widgets/favorite_list_widget.dart';
