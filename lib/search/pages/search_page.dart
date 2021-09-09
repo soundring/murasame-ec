297ccff4db87631af9334753a40cdbacc77882f5
soundring <soundringing@yahoo.co.jp>
2021-09-09 18:04:19 +0900

    :new:検索ページを作成

--- lib/search/pages/search_page.dart
+++ lib/search/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:murasame_ec/search/widgets/widgets.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -6,17 +7,9 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "検索",
-              hintText: "検索ワードを入力",
-            ),
-          ),
-        )
+      children: const [
+        SearchBoxWidget(),
+        SearchListWidget(),
       ],
     );
   }
