54634d6b24fa488b478285ac829098aa40ffc5c9
soundring <soundringing@yahoo.co.jp>
2021-09-19 03:39:17 +0900

    :wrench:カートにアイテムがないときに購入ボタンを押せないように

--- lib/cart/widgets/purchase_button_widget.dart
+++ lib/cart/widgets/purchase_button_widget.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:murasame_ec/cart/model/model.dart';
 
 class PurchaseButtonWidget extends HookWidget {
   const PurchaseButtonWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isExistCartItem =
+        useProvider(cartProvider.select((s) => s.summary.quantity == 0));
+
     return Container(
       alignment: Alignment.center,
       child: ElevatedButton(
@@ -13,8 +19,8 @@ class PurchaseButtonWidget extends HookWidget {
           fixedSize: Size(MediaQuery.of(context).size.width * 0.8,
               MediaQuery.of(context).size.height * 0.07),
         ),
-        onPressed: () {},
-        child: Text(
+        onPressed: isExistCartItem ? null : () {},
+        child: const Text(
           '購入する',
         ),
       ),
